73902707ec838c8d307a00f44e71a65dda663fa4
unknown <mrazzakov@gmail.com>
2019-03-09 23:57:32 -0600

    ch 3 - added Nav and Footer into external files; organize App.js

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav>
+            <h1>This is My NavBar</h1>
+            <p>This is my paragraph to the Navbar</p>
+        </nav>
+    );
+}
+
+export default Nav

7138ec4cc3da7bdb74781021579c84b79aa6869b
unknown <mrazzakov@gmail.com>
2019-03-09 23:28:31 -0600

    ch2 - another component practice

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function MyApp() {
-    return  (<div>
-                <h1>Put DOM in js function</h1>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
-            </div>);
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Mike Razzakov</h1>
+            <p>This is my first attempt with practicing with ReAct and these are my top 3 vacation spots</p>
+            <ol>
+                <li>Dominican Republic</li>
+                <li>Mexico</li>
+                <li>Disney, FL</li>
+            </ol>
+        </div>
+    );
 }
 
-ReactDOM.render(<MyApp/>, document.getElementById('root'));
+ReactDOM.render(<MyInfo/>,document.getElementById('root'));
 
 
-{/* <App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister(); */}
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
